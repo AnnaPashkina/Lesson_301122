b2a9ab24bd81311c7f6c9caff5872e6eafa0df9e
AnnaPashkina <anya230489@gmail.com>
2022-12-05 22:06:38 +0200

    Update hometask21.sql
    ciflict

--- hometask21.sql
+++ hometask21.sql
@@ -1,7 +1,3 @@
-select * from homework.users;
-insert into homework.users (id, email, password, createdAt, updatedAt, role)
-values (1, "Petrenko@gmail.com", "Password123", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, "user");
-insert into homework.car_brands (id, title, createdAt, updatedAt)
 values (1, "Audi", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 select * from homework.car_brands;
 insert into homework.car_models (id, carBrandId, title, createdAt, updatedAt)
@@ -14,6 +10,3 @@ update homework.cars
 set mileage = 15;
 select * from homework.cars;
 select * from homework.users;
-insert into homework.expenses (id, carId, repotedAt, mileage, liters, totalCost, CreatedAt, updatedAt)
-values (1, 1, "2022-11-18", 18, 27, 1000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-select * from homework.expenses;
